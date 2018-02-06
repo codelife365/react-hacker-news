c3e22bd9910c911a86c5398c8f1b2a7808ce45d0
codelife365 <codelife365@163.com>
2018-02-07 02:41:49 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,69 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+  state = { topIds: [], items: {} }
+
+  async componentDidMount() {
+    let items = {}
+    const res = await fetch(
+      'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
+    )
+    const topIdsAll = await res.json()
+    const topIds = topIdsAll.slice(0, 5)
+
+    const results = topIds.map(async id => {
+      const res = await fetch(
+        `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
+      )
+      const item = await res.json()
+      items[id] = item
+    })
+
+    Promise.all(results).then(() => {
+      this.setState({ topIds, items })
+    })
+  }
+
   render() {
+    console.log(this.state)
+    const { topIds, items } = this.state
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Hacker News</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main className="App-intro">
+          <ul>
+            {topIds.map((id, index) => {
+              const item = items[id]
+              const now = Date.now()
+              return (
+                <li key={id}>
+                  {index + 1}
+                  {'. '}
+                  <a href={item.url}>{item.title}</a>
+                  <span style={{ color: '#666' }}>
+                    ({new URL(item.url).hostname})
+                  </span>
+
+                  <p style={{ marginLeft: 10 }}>
+                    {item.score} points by texodus{' '}
+                    {parseInt(
+                      (Number(String(Date.now()).slice(0, -3)) - item.time) /
+                        3600
+                    )}{' '}
+                    hour ago | hide | {item.descendants}
+                    comments
+                  </p>
+                </li>
+              )
+            })}
+          </ul>
+        </main>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
